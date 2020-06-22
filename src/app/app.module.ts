2b40dce852d204d1d8a71872d5e79127b11a818a
ankit binjola <abinjola.binjola@gmail.com>
2020-06-22 18:47:08 +0530

    added firebase auth services and some modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
+import { environment } from '../environments/environment'
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingupComponent } from './pages/singup/singup.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+//import form and httpclient
+
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+//importing firebase auth
+
+import {AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireModule } from '@angular/fire';
+
+
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Toastr module
+import {ToastrModule} from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReposComponent,
+    UserCardComponent,
+    HomeComponent,
+    SingupComponent,
+    SignInComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCF5jpDX3jQH9-qoGasn_D3uhs93UEDdcs",
+    authDomain: "api-project-920057595806.firebaseapp.com",
+    databaseURL: "https://api-project-920057595806.firebaseio.com",
+    projectId: "api-project-920057595806",
+    storageBucket: "api-project-920057595806.appspot.com",
+    messagingSenderId: "920057595806",
+    appId: "1:920057595806:web:168acd4633edfd1c34788f"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
